df1547155227fcb29b9d24b6cfb39203b5079677
Lucia Cyprianova <l.cyprianova@gmail.com>
2021-01-08 13:57:18 +0100

    drag existing points

--- main.js
+++ main.js
@@ -1,16 +1,52 @@
 let points = [];
 let newPoint = [];
+let cursorOnPt = false;
 
 const lineGenerator = d3.line();
 let pathData = lineGenerator(points);
 
 // events
-let svg = d3.select("svg");
-svg.on("click", function (d) {
-  newPoint = [d.layerX, d.layerY];
-  points.push(newPoint);
+let dragHandler = d3.drag().on("drag", function (d) {
+  let circle = d3.select(this);
+  circle.attr("cx", d.x).attr("cy", d.y);
+  let ptIndex = parseInt(circle.attr("id").slice(5));
+  points[ptIndex] = [d.x, d.y];
   updateGeometry();
 });
+// add new point on click in svg
+let svg = d3.select("svg");
+svg.on("click", function (d) {
+  if (!cursorOnPt) {
+    newPoint = [d.layerX, d.layerY];
+    points.push(newPoint);
+    updateGeometry();
+  }
+});
+
+function registerPointEvents() {
+  let circles = d3.select(".points").selectAll("circle");
+  // hover on circles
+  circles
+    .on("mouseover", function () {
+      d3.select(this)
+        .transition()
+        .duration(100)
+        .attr("r", 6)
+        .attr("fill", "purple");
+      cursorOnPt = true;
+    })
+    .on("mouseout", function () {
+      d3.select(this)
+        .transition()
+        .duration(100)
+        .attr("r", 4)
+        .attr("fill", "white");
+      cursorOnPt = false;
+    });
+
+  //call drag handler
+  dragHandler(circles);
+}
 
 // function definitions
 function updateGeometry() {
@@ -35,6 +71,7 @@ function updateCircles() {
     })
     .attr("r", 4);
   circles.exit().remove();
+  registerPointEvents();
 }
 
 function updatePolyline() {
