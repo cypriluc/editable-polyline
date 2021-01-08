eb9aff43d56f9667fac2c2c7640a0effa863e8dd
Lucia Cyprianova <l.cyprianova@gmail.com>
2021-01-08 18:39:54 +0100

    limit drag to svg borders

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
+// global constants
+const svgWidth = d3.select("svg").attr("width");
+const svgHeight = d3.select("svg").attr("height");
+const lineGenerator = d3.line();
+const pointRadius = 4;
+const pointRadiusHover = 6;
+
+// global variables
 let points = [];
 let newPoint = [];
 let cursorOnPt = false;
+let pathData;
 
-const lineGenerator = d3.line();
-let pathData = lineGenerator(points);
-
-// events
-let dragHandler = d3.drag().on("drag", function (d) {
-  let circle = d3.select(this);
-  circle.attr("cx", d.x).attr("cy", d.y);
-  let ptIndex = parseInt(circle.attr("id").slice(5));
-  points[ptIndex] = [d.x, d.y];
-  updateGeometry();
-});
-// add new point on click in svg
+// register event - add new point on click in svg
 let svg = d3.select("svg");
 svg.on("click", function (d) {
   if (!cursorOnPt) {
@@ -23,30 +21,33 @@ svg.on("click", function (d) {
   }
 });
 
-function registerPointEvents() {
-  let circles = d3.select(".points").selectAll("circle");
-  // hover on circles
-  circles
-    .on("mouseover", function () {
-      d3.select(this)
-        .transition()
-        .duration(100)
-        .attr("r", 6)
-        .attr("fill", "purple");
-      cursorOnPt = true;
-    })
-    .on("mouseout", function () {
-      d3.select(this)
-        .transition()
-        .duration(100)
-        .attr("r", 4)
-        .attr("fill", "white");
-      cursorOnPt = false;
-    });
+// register event - drag existing point
+let dragHandler = d3.drag().on("drag", function (d) {
+  let circle = d3.select(this);
+  let ptIndex = parseInt(circle.attr("id").slice(5));
+  let newX;
+  let newY;
 
-  //call drag handler
-  dragHandler(circles);
-}
+  if (d.x < svgWidth - pointRadius && d.x > pointRadius) {
+    newX = d.x;
+  } else if (d.x >= svgWidth - pointRadius) {
+    newX = svgWidth - pointRadius;
+  } else {
+    newX = pointRadius;
+  }
+
+  if (d.y < svgHeight - pointRadius && d.y > pointRadius) {
+    newY = d.y;
+  } else if (d.y >= svgHeight - pointRadius) {
+    newY = svgHeight - pointRadius;
+  } else {
+    newY = pointRadius;
+  }
+
+  points[ptIndex] = [newX, newY];
+  circle.attr("cx", newX).attr("cy", newY);
+  updateGeometry();
+});
 
 // function definitions
 function updateGeometry() {
@@ -69,9 +70,35 @@ function updateCircles() {
     .attr("cy", function (d) {
       return d[1];
     })
-    .attr("r", 4);
+    .attr("r", pointRadius)
+    .attr("fill", "white");
   circles.exit().remove();
   registerPointEvents();
+}
+
+function registerPointEvents() {
+  let circles = d3.select(".points").selectAll("circle");
+  // hover on circles
+  circles
+    .on("mouseover", function () {
+      d3.select(this)
+        .transition()
+        .duration(100)
+        .attr("r", pointRadiusHover)
+        .attr("fill", "purple");
+      cursorOnPt = true;
+    })
+    .on("mouseout", function () {
+      d3.select(this)
+        .transition()
+        .duration(100)
+        .attr("r", pointRadius)
+        .attr("fill", "white");
+      cursorOnPt = false;
+    });
+
+  //call drag handler
+  dragHandler(circles);
 }
 
 function updatePolyline() {
